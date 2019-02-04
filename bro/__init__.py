0eece884b9f8436c5e717d49c0486398fe896475
xuzuoyang <xuzuoyang@gmail.com>
2019-02-04 12:28:40 +0800

    kickstart based on mypackage

---
+++ bro/__init__.py
@@ -0,0 +1,5 @@
+from .core import *
+from .api import *
+
+
+__all__ = []
---
+++ tests/test_dict_functions.py
@@ -0,0 +1,12 @@
+from .context import bro
+from bro import dict_substract
+
+
+class TestDictUtils():
+    '''Test dict related functions.
+    '''
+    def test_substract(self):
+        dict1 = {'a': 11, 'b': 0}
+        dict2 = {'b': 0}
+
+        assert dict_substract(dict1, dict2) == {'a': 11}
