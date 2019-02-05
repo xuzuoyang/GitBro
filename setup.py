4e221d6a6dc82eb7447b06e7e13396afb5ea91a4
xuzuoyang <xuzuoyang@gmail.com>
2019-02-05 15:59:51 +0800

    setup and launch v0.0.1

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 
 from setuptools import find_packages, setup
 
-# Package meta-data.
-NAME = 'bro'
-DESCRIPTION = 'Personal python package project template.'
-URL = 'https://github.com/xuzuoyang/bro'
-EMAIL = 'xzyduoduo@126.com'
+NAME = 'gitbro'
+DESCRIPTION = 'Lightweight cli tool for pull request management.'
+URL = 'https://github.com/xuzuoyang/gitbro'
+EMAIL = 'xuzuoyang@gmail.com'
 AUTHOR = 'xzy'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = None
 
-REQUIRED = []
+REQUIRED = ['requests', 'click', 'tabulate']
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
     with open(os.path.join(here, NAME, '__version__.py')) as f:
@@ -35,23 +30,10 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['bro'],
-
     entry_points={
         'console_scripts': ['bro=bro.cli:bro'],
     },
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ]
+    license='MIT'
 )
